226cd438b6cb0fa3367508e913f4ae84801ac813
alirezah95 <alirezahassani95@gmail.com>
2022-12-07 16:58:39 +0330

    refactor: make items readonly, add data for item data

--- treeitem.cpp
+++ treeitem.cpp
@@ -44,12 +44,8 @@ QQmlListProperty<TreeItem> TreeItem::items()
     return {
         this,
         this,
-        &TreeItem::appendTreeItem,
         &TreeItem::countTreeItems,
         &TreeItem::treeItem,
-        &TreeItem::clearTreeItems,
-        &TreeItem::replaceTreeItem,
-        &TreeItem::removeLastTreeItem,
     };
 }
 
@@ -102,10 +98,11 @@ void TreeItem::clear()
     return mChildItems.clear();
 }
 
-void TreeItem::appendTreeItem(QQmlListProperty<TreeItem>* list, TreeItem* item)
+void TreeItem::setData(const QJsonObject& newData)
 {
-    reinterpret_cast<TreeItem*>(list->data)->appendChildItem(item);
-    return;
+    if (mData == newData)
+        return;
+    mData = newData;
 }
 
 qsizetype TreeItem::countTreeItems(QQmlListProperty<TreeItem>* list)
@@ -116,23 +113,4 @@ qsizetype TreeItem::countTreeItems(QQmlListProperty<TreeItem>* list)
 TreeItem* TreeItem::treeItem(QQmlListProperty<TreeItem>* list, qsizetype index)
 {
     return reinterpret_cast<TreeItem*>(list->data)->child(index);
-}
-
-void TreeItem::clearTreeItems(QQmlListProperty<TreeItem>* list)
-{
-    reinterpret_cast<TreeItem*>(list->data)->clear();
-}
-
-void TreeItem::replaceTreeItem(
-    QQmlListProperty<TreeItem>* list, qsizetype index, TreeItem* item)
-{
-    reinterpret_cast<TreeItem*>(list->data)->replaceChildItem(index, item);
-    return;
-}
-
-void TreeItem::removeLastTreeItem(QQmlListProperty<TreeItem>* list)
-{
-    auto item = reinterpret_cast<TreeItem*>(list->data);
-    item->removeChildItem(item->childCount() - 1);
-    return;
 }
--- treeitem.h
+++ treeitem.h
@@ -1,6 +1,7 @@
 #ifndef TREEITEM_H
 #define TREEITEM_H
 
+#include <QJsonObject>
 #include <QObject>
 #include <QQmlListProperty>
 #include <QVariant>
@@ -9,7 +10,8 @@
 class TreeItem : public QObject
 {
     Q_OBJECT
-    Q_PROPERTY(QQmlListProperty<TreeItem> items READ items NOTIFY itemsChanged)
+    Q_PROPERTY(QJsonObject data READ data WRITE setData)
+    Q_PROPERTY(QQmlListProperty<TreeItem> items READ items CONSTANT)
     Q_CLASSINFO("DefaultProperty", "items")
 
 public:
@@ -31,23 +33,18 @@ public:
     void replaceChildItem(qsizetype index, TreeItem* newItem);
     void clear();
 
-signals:
-    void itemsChanged();
+    QJsonObject& data() { return mData; }
+    void setData(const QJsonObject& newData);
 
 private:
-    static void appendTreeItem(
-        QQmlListProperty<TreeItem>* list, TreeItem* item);
     static qsizetype countTreeItems(QQmlListProperty<TreeItem>* list);
     static TreeItem* treeItem(
         QQmlListProperty<TreeItem>* list, qsizetype index);
-    static void clearTreeItems(QQmlListProperty<TreeItem>* list);
-    static void replaceTreeItem(
-        QQmlListProperty<TreeItem>* list, qsizetype index, TreeItem* item);
-    static void removeLastTreeItem(QQmlListProperty<TreeItem>* list);
 
 private:
     TreeItem* mParentItem;
     QVector<TreeItem*> mChildItems;
+    QJsonObject mData;
 };
 
 #endif // TREEITEM_H
